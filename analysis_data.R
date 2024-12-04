3e0304067386f12ab00ffa515b014a09ada3904c
Nambinin <namb.michel@gmail.com>
2024-12-04 02:22:34 +0100

    update about exercice until g-ii

--- analysis_data.R
+++ analysis_data.R
@@ -1,4 +1,4 @@
-#Load csv file
+#a - Load csv file
 data <- read.csv("DataDefaults.csv")
 
 #show the head of the data
@@ -20,38 +20,48 @@ var_x <- sum( (x-x_bar)^2 ) / (n-1)
 var_y <- sum( (y-y_bar)^2 ) / (n-1)
 covar_x_y <- sum( (x-x_bar)*(y-y_bar) )/ (n-1)
 
+#b - estimate correlation
 cor_x_y <- covar_x_y / (var_x*var_y)^0.5
 cor_x_y
 
+#check with function cor()
 cor(x, y)
 
-# Estimate the slope
-beta_1 <- sum( (x - x_bar)* y_bar)/ sum( (x - x_bar)^2)
-beta1 <- covar_x_y / var_x
-beta_1
-beta1
-
-sd_x <- sqrt(var_x)
-sd_y <- sqrt(var_y)
-beta1_check <- cor_x_y * (sd_y / sd_x)
-all.equal(beta1, beta1_check)  # Should return TRUE
-
+#c- Estimate the slope
+beta_1 <- sum( (x - x_bar)* y)/ sum( (x - x_bar)^2)
 
 #estimate the intercept
 beta_zero <- y_bar-(beta_1 * x_bar)
 
-beta_1
-beta_zero
+beta_1 ;beta_zero
 
-#estimate the variance parameter sigma
-y_hat <- beta_zero + beta_1 * x
+#d- relationship between estimated slope and correlation coefficient
+correlation = beta_1 * ((var_x^0.5) / (var_y^0.5))
+correlation
+cor_x_y
 
+#e - estimate the variance parameter sigma
 sigma2 <- sum( (y-beta_zero-(beta_1*x))^2 ) / (n-2)
 sigma2
 
+#f - Estimate the covariance matrix
 
-#model using lm() function
-model <- lm(Earnings ~ defaults)
+var_beta_1 <- sigma2 / sum((x - x_bar)^2)
+var_beta_0 <- sigma2 * sum(x^2) / n* sum((x - x_bar)^2)
+covar_beta <- -x_bar * sigma2 / sum((x - x_bar)^2)
 
-plot(defaults, Earnings,xlab = "x", ylab="y", pch=19, col="blue")
-abline(model, col="red", lwd=2)
+cov_matrix <- matrix(
+  c(var_beta_0, covar_beta,
+    covar_beta, var_beta_1),
+  nrow = 2, byrow = TRUE
+)
+cov_matrix
+
+#g - i - Estimate the expected default rate for this applicant
+y_new <- beta_zero + (beta_1 * 286510)
+y_new
+
+#g- ii - Estimate the standard error around this expected default rate
+SE <- var(beta_zero+beta_1*x)
+SE
+
